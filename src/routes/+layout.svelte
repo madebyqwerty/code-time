fa51221dfb19cfe40156e8b3945531901ec5d0d8
Tomáš Kebrle <dev@tomaskebrle.cz>
2023-01-07 20:02:01 +0100

    Vylepšit input a registraci

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,6 @@
 <script>
 	import '../app.scss';
+	import 'iconify-icon';
 </script>
 
 <main class="bg-ctgray-500 font-fira-code text-white min-h-screen">
--- src/routes/auth/login/+page.svelte
+++ src/routes/auth/login/+page.svelte
@@ -1,21 +1,27 @@
 <script lang="ts">
-	import { goto } from '$app/navigation';
 	import { login } from '$lib/pocketbase';
 	import Wrapper from '../Wrapper.svelte';
 	import Button from '$lib/components/Button.svelte';
 	import Input from '$lib/components/forms/Input.svelte';
 
-	let email: string, password: string;
+	let email: string,
+		password: string = '';
 </script>
 
 <Wrapper>
-	<form class="wrapper">
+	<form class="wrapper" on:submit={() => login(email, password)}>
 		<h1 class="width-max white">LOGIN</h1>
 		<a class="text-base grey grey-hover" href="/auth/register"
 			>Ještě nemáte účet? Zde si ho vytvořte</a
 		>
-		<Input type="text" bind:value={email} placeholder="E-mail" />
-		<Input type="password" bind:value={password} placeholder="Heslo" />
+		<Input type="text" bind:value={email} placeholder="jmeno@priklad.cz" label="E-mail" />
+		<Input
+			type="password"
+			bind:value={password}
+			placeholder="*****"
+			showPasswordSwitch={true}
+			label="Heslo"
+		/>
 		<Button>Přihlásit se</Button>
 	</form>
 </Wrapper>
