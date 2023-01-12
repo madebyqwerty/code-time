e669c2ec951ce3dafd6ed34e9d10ade8762b8609
Venca321 <login@vaclavparma.cz>
2023-01-12 17:12:03 +0100

    Vytvořil jsem admin UI

---
+++ src/routes/databaseDev/+page.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+	import { createRecord } from '$lib/pocketbase';
+
+	export let id;
+	export let min = 1;
+	export let max = 25;
+	export let step = 1;
+	export let users = 5;
+
+	export let id2;
+	export let tags = 5;
+
+	function handleClick() {
+		console.log(users, tags)
+	}
+</script>
+
+Users: {users}
+<input
+  type="range"
+  {id} {min} {max} {step}
+  name="{id}"
+  bind:value={users}
+/>
+
+<br>
+
+Tags: {tags}
+<input
+  type="range"
+  {id2} {min} {max} {step}
+  name="{id2}"
+  bind:value={tags}
+/>
+
+<br>
+
+<button on:click={handleClick}>Zaplnit databázy</button>
+
