7e7eb91bb37d0d571cce0ee437018c5b525b49b4
Tomáš Kebrle <dev@tomaskebrle.cz>
2023-01-23 17:58:43 +0100

    Přidat filtrování pomocí obtížnosti

--- src/routes/Sidenav/Stars.svelte
+++ src/routes/Sidenav/Stars.svelte
@@ -1,32 +1,49 @@
 <script lang="ts">
+	import { goto, invalidate } from '$app/navigation';
 	import { page } from '$app/stores';
 	import Checkbox from './Checkbox.svelte';
 
 	const starsArr = ['*', '**', '***', '****', '*****'];
 
+	let stars: number[];
 	$: starsParam = $page.url.searchParams.get('stars');
 	$: stars = starsParam ? JSON.parse(starsParam) : [];
 
-	const handleStarsChange = (starCount: number, checked: boolean) => {
+	const handleStarsChange = async (starCount: number, checked: boolean) => {
 		const url = new URL(window.location.href);
-		url.searchParams.set('stars', JSON.stringify(..stars, starCount));
+		if (checked) {
+			url.searchParams.set('stars', JSON.stringify([...stars, starCount]));
+		} else {
+			url.searchParams.set('stars', JSON.stringify(stars.filter((star) => star !== starCount)));
+		}
+
+		await goto(url);
+		await invalidate('home');
 	};
 </script>
 
 <section>
 	<h3>Obtížnost</h3>
-	{#each starsArr as star}
-		<Checkbox
-			active={stars.includes(star)}
-			--bg="#fff"
-			on:check={(e) => handleStarsChange(star, e.detail)}
-		/>{tag.name}
-		<div class="star">{star}</div>
+	{#each starsArr as star, starIndex}
+		<div class="checkbox-wrapper">
+			<Checkbox
+				active={stars.includes(starsArr.indexOf(star))}
+				--bg="#fff"
+				on:check={(e) => handleStarsChange(starIndex, e.detail)}
+			/>
+			<div class="star">{star}</div>
+		</div>
 	{/each}
 </section>
 
 <style>
 	.star {
 		font-family: 'Silkscreen', monospace;
+	}
+
+	.checkbox-wrapper {
+		display: flex;
+		align-items: center;
+		gap: 0.4rem;
 	}
 </style>
