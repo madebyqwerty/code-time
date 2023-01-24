bd713fe193422dd67296099c1ac89ceba9ae6743
Tomáš Kebrle <dev@tomaskebrle.cz>
2023-01-24 16:02:37 +0100

    Začít pracovat na menu

---
+++ src/routes/Sidenav/UserMenu.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import { Menu, MenuButton, MenuItems, MenuItem } from '@rgossiaux/svelte-headlessui';
+	import { currentUser } from '$lib/pocektbase';
+</script>
+
+<Menu>
+	<MenuButton />
+</Menu>
+
+<style lang="scss">
+</style>
