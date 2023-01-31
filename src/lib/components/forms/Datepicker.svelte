4e57de5f8d5188329a6e80a01bd025f03e6c3b30
Tomáš Kebrle <dev@tomaskebrle.cz>
2023-01-31 15:43:37 +0100

    Fixnout přidávání záznamu

---
+++ src/lib/components/forms/Datepicker.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+	import flatpickr from 'flatpickr';
+	import { Czech } from 'flatpickr/dist/l10n/cs';
+	import type { Instance } from 'flatpickr/dist/types/instance';
+	import { onMount } from 'svelte';
+
+	let datepicker;
+
+	export let value: Date;
+
+	onMount(() => {
+		datepicker = flatpickr('.flatpickr-create', {
+			mode: 'single',
+			locale: Czech,
+			onClose: (e) => (value = e[0]),
+			defaultDate: new Date()
+		}) as Instance;
+	});
+</script>
+
+<input
+	class="flatpickr-create flatpickr-input"
+	id="datepicker"
+	type="text"
+	placeholder="Vyber rozmezí"
+	data-id="range"
+	readonly={true}
+/>
+
+<style lang="scss">
+	input {
+		color: $green-lightest;
+		border: 1px solid $green-primary;
+		background: transparent;
+		padding: 0.8rem;
+		display: block;
+		width: 100%;
+	}
+</style>
