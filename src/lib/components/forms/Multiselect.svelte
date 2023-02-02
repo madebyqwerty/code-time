43674a78254cbde8abc92bdea55dfff5b7f7976f
Tomáš Kebrle <dev@tomaskebrle.cz>
2023-02-02 17:09:13 +0100

    Fix vite complaining

--- src/lib/components/forms/Multiselect.svelte
+++ src/lib/components/forms/Multiselect.svelte
@@ -17,27 +17,3 @@
 	<LanguageSlot let:option {option} slot="selected" />
 	<LanguageSlot let:option {option} slot="option" />
 </MultiSelect>
-
-<style lang="scss">
-	:global(div.multiselect > ul.options) {
-		@include scrollbars(10px, $green-primary, lighten($background, 5));
-		background: lighten($background, 5);
-		border-radius: 0px;
-		z-index: 150 !important;
-	}
-	:global(div.multiselect > ul.selected > li) {
-		background: lighten($background, 10);
-	}
-
-	:global(div.multiselect) {
-		border: 1px solid $green-primary;
-		background-color: transparent;
-		padding: 0.8rem;
-		color: $green-lightest;
-		border-radius: 0;
-	}
-
-	:global(div.multiselect input::placeholder) {
-		color: $green-lightest;
-	}
-</style>
