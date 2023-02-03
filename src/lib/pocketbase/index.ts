479b854514c8efc391475ca3b04ea1d0fddad91a
Tomáš Kebrle <dev@tomaskebrle.cz>
2023-02-03 10:14:36 +0100

    Fixnout hardkodovani api route

--- src/lib/pocketbase/index.ts
+++ src/lib/pocketbase/index.ts
@@ -2,9 +2,7 @@ import { goto, invalidate } from '$app/navigation';
 import PocketBase, { Admin, Record } from 'pocketbase';
 import { writable } from 'svelte/store';
 
-const pocketBaseURL = import.meta.env.DEV
-	? 'http://localhost'
-	: 'http://public-a4ced53e5ce70b738f88b70113bc806d.tourdeapp.cz/';
+const pocketBaseURL = import.meta.env.DEV ? 'http://localhost' : '/';
 export const pb = new PocketBase(pocketBaseURL);
 
 export const currentUser = writable<Record | Admin>(pb.authStore.model!);
